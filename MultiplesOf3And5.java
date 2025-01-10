54ca5e9952701080593cca4a18fd2ddf394d8dfd
Csharp789 <96018319+Csharp789@users.noreply.github.com>
2025-01-10 22:59:54 +0000

    new changes

--- MultiplesOf3And5.java
+++ MultiplesOf3And5.java
@@ -1,9 +1,9 @@
 public class MultiplesOf3And5 {
     public static void main(String[] args) {
-        int limit = 1000;
+        int thelimit = 1000;
         int totalsum = 0;
 
-        for (int i = 0; i < limit; i++) {
+        for (int i = 0; i < thelimit; i++) {
             if (i % 3 == 0 || i % 5 == 0) {
                 totalsum += i;
             }
